9fe24713847d99b6fad01455eff98d1780754361
Felipe Lagos <felagossanchez@gmail.com>
2024-12-13 21:20:19 -0300

    Refactor build configuration for order-service modules and remove unused files

--- order-service/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-// Since everything is already configured in the root build.gradle.kts,
-// and this service doesn't have any specific dependencies or configurations,
-// we can leave this file nearly empty.
-
-// Empty plugins block is still needed to properly apply the plugins
-plugins {
-}
