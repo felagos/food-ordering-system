4e41bfe21cf6ef1c010486f3ac35ee1fca3feb4e
Felipe Lagos <felagossanchez@gmail.com>
2024-12-29 18:37:29 -0300

    Add BaseEntity and AggregateRoot classes for domain model structure

---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/entity/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.food.ordering.system.domain.entity;
+
+import java.util.Objects;
+
+public abstract class BaseEntity<T> {
+
+    private T id;
+
+    public T getId() {
+        return id;
+    }
+
+    public void setId(T id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity<?> that = (BaseEntity<?>) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
