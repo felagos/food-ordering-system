ef74322f3b5b70db5f018e25cdf6af82dbddf3e4
Felipe Lagos <felagossanchez@gmail.com>
2025-01-05 19:11:11 -0300

    Add order domain core module with Order, OrderItem, Product entities and value objects

---
+++ order-service/order-domain/order-domain-core/src/main/java/valueobject/OrderItemId.java
@@ -0,0 +1,11 @@
+package valueobject;
+
+import com.food.ordering.system.domain.valueobject.BaseId;
+
+public class OrderItemId extends BaseId<Long> {
+
+    public OrderItemId(Long id) {
+        super(id);
+    }
+
+}
---
+++ order-service/order-domain/order-domain-core/src/main/java/valueobject/TrackingId.java
@@ -0,0 +1,11 @@
+package valueobject;
+
+import com.food.ordering.system.domain.valueobject.BaseId;
+
+public class TrackingId extends BaseId<Long> {
+
+    public TrackingId(Long id) {
+        super(id);
+    }
+
+}
