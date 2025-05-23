544ba7c2852facfc21c2e901d51c4f258a5823bc
Felipe Lagos <felagossanchez@gmail.com>
2025-05-23 16:51:43 -0400

    Refactor exception handling by renaming Domainxception to DomainException and updating references

--- src/main/java/src/main/java/order_domain/core/exception/OrderDomainException.java
+++ src/main/java/src/main/java/order_domain/core/exception/OrderDomainException.java
@@ -1,8 +1,8 @@
 package src.main.java.order_domain.core.exception;
 
-import src.main.java.common.domain.exception.Domainxception;
+import src.main.java.common.domain.exception.DomainException;
 
-public class OrderDomainException extends Domainxception {
+public class OrderDomainException extends DomainException {
 
     public OrderDomainException(String message) {
         super(message);
